5fc0399947b327b12a3193d7a45a3ee80358dfec
James Clarke <james.clarke@tcu.edu>
2024-01-14 18:06:14 -0600

    feat: openssl issue fix

--- script.py
+++ script.py
@@ -1,8 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 
+
+def url_gen(page_number):
+    url = f"https://uk.trustpilot.com/review/dreamcargiveaways.co.uk?page={page_number}"
+    return url
+
+
+def parse(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15"
+    }
+    resp = requests.get(url, headers=headers)
+    html = resp.content
+    soup = BeautifulSoup(html, "lxml")
+    return soup
+
+
 def main():
-    print("Hello!")
+    url = url_gen(1)
+    print(url)
+    
 
 
 if __name__ == "__main__":
